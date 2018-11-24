d7b05e51161051b9cbff39f079ffc7b936f25194
Richard Taylor Dawson <richard.taylor.dawson@gmail.com>
2018-11-24 16:01:50 -0700

    Gradient background added. Calculator class started and nearly setup

--- assets/js/calculator.js
+++ assets/js/calculator.js
@@ -1,10 +1,134 @@
 class Calculator {
-    constructor() {
-        this.currentValue = height;
-        this.width = width;
+    constructor(elements) {
+        // Number buttons zero indexed 0-9
+        this.numberButtons = elements.numberButtons;
+
+        this.decimalButton = elements.decimalButton;
+
+        this.clearButton = elements.clearButton;
+
+        this.additionButton = elements.additionButton;
+        this.subtractionButton = elements.subtractionButton;
+        this.multiplicationButton = elements.multiplicationButton;
+        this.divisionButton = elements.divisionButton;
+
+        this.equalsButton = elements.equalsButton;
+
+        this.primaryDisplay = elements.primaryDisplay;
+        this.secondaryDisplay = elements.secondaryDisplay;
+
+        this.initializeClickEvents();
+
+        this.initializeKeyboardEvents();
     }
 
-    static userInput(value) {
+    initializeClickEvents() {
+        // Numbers 0 - 9
+        for(let i = 0; i < this.numberButtons.length; i++) {
+            this.numberButtons[i].addEventListener('click', (event) => {
+                this.input(this.getInputValue(event.target));
+            });
+        }
+
+        this.decimalButton.addEventListener('click', () => {
+            this.input('.');
+        });
+
+        this.clearButton.addEventListener('click', () => {
+            this.clear();
+        });
+
+        this.additionButton.addEventListener('click', () => {
+            this.add();
+        });
+
+        this.subtractionButton.addEventListener('click', () => {
+            this.subtract();
+        });
+
+        this.multiplicationButton().addEventListener('click', () => {
+            this.multiply();
+        });
+
+        this.divisionButton.addEventListener('click', () => {
+            this.divide();
+        });
+
+        this.equalsButton.addEventListener('click', () => {
+            this.compute();
+        });
+    }
+
+    initializeKeyboardEvents() {
+        document.addEventListener('keydown', (event)  => {
+            console.log(event.keyCode);
+
+            switch(event.keyCode) {
+                // 48 - 57
+                // Numbers 0 - 9
+                case 48:
+                    this.input(0);
+                    break;
+                case 49:
+                    this.input(1);
+                    break;
+                case 50:
+                    this.input(2);
+                    break;
+                case 51:
+                    this.input(3);
+                    break;
+                case 52:
+                    this.input(4);
+                    break;
+                case 53:
+                    this.input(5);
+                    break;
+                case 54:
+                    this.input(6);
+                    break;
+                case 55:
+                    this.input(7);
+                    break;
+                case 56:
+                    this.input(8);
+                    break;
+                case 57:
+                    this.input(9);
+                    break;
+                // . or decimal key
+                case 190:
+                    this.input('.');
+                    break;
+                // Escape key used as the clear button
+                case 27:
+                    this.clear();
+                    break;
+                //TODO finish these later. May have to change everything to a map
+                // case 187:
+                //     this.add();
+                //     break;
+                // case 187:
+                //     this.subtract();
+                //     break;
+                // case 187:
+                //     this.multiply();
+                //     break;
+                // case 191:
+                //     this.divide();
+                //     break;
+                // case 191:
+                //     this.compute();
+                //     break;
+            }
+        });
+    }
+
+    getInputValue(element) {
+        return this.numberButtons.indexOf(element);
+    }
+
+    input(value) {
         document.getElementById('number').innerText = (document.getElementById('number').innerText === '0')
             ? value.toString()
             : document.getElementById('number').innerText.toString() + value.toString();
@@ -14,20 +138,33 @@ class Calculator {
             : document.getElementById('sub-number').innerText.toString() + value.toString();
     }
 
-
-    static add(a, b) {
-        return a + b;
+    clear() {
+        this.primaryDisplay.innerText = '0';
+        this.secondaryDisplay.innerText = '0';
     }
 
-    static subtract(a, b) {
-        return a - b;
+    add() {
+        this.primaryDisplay.innerText = '0';
+        this.secondaryDisplay.innerText = this.secondaryDisplay.innerText + ' + ';
     }
 
-    static multiply(a, b) {
-        return a * b;
+    subtract() {
+        this.primaryDisplay.innerText = '0';
+        this.secondaryDisplay.innerText = this.secondaryDisplay.innerText + ' - ';
     }
 
-    static divide(a, b) {
-        return a / b;
+    multiply() {
+        this.primaryDisplay.innerText = '0';
+        this.secondaryDisplay.innerText = this.secondaryDisplay.innerText + ' * ';
     }
+
+    divide() {
+        this.primaryDisplay.innerText = '0';
+        this.secondaryDisplay.innerText = this.secondaryDisplay.innerText + ' / ';
+    }
+
+    compute() {
+
+    }
+
 }
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,92 +1,24 @@
-let numberButtons = document.getElementsByClassName('btn-number');
-
-for(let i = 0; i < numberButtons.length; i++) {
-    numberButtons[i].addEventListener('click', function() {
-        input(this.value);
-    });
-}
-
-document.getElementById('btn-clear').addEventListener('click', function() {
-    document.getElementById('number').innerText = '0';
-    document.getElementById('sub-number').innerText = '0';
+const calculator = new Calculator({
+    "numberButtons": [
+        document.getElementById('zeroButton'),
+        document.getElementById('oneButton'),
+        document.getElementById('twoButton'),
+        document.getElementById('threeButton'),
+        document.getElementById('fourButton'),
+        document.getElementById('fiveButton'),
+        document.getElementById('sixButton'),
+        document.getElementById('sevenButton'),
+        document.getElementById('eightButton'),
+        document.getElementById('nineButton')
+    ],
+    "decimalButton" : document.getElementById('btn-decimal'),
+    "clearButton" : document.getElementById('btn-clear'),
+    "additionButton" : document.getElementById('btn-addition'),
+    "subtractionButton" : document.getElementById('btn-subtraction'),
+    "multiplicationButton" : document.getElementById('btn-multiplication'),
+    "divisionButton" : document.getElementById('btn-division'),
+    "equalsButton" : document.getElementById('btn-equals'),
+    "primaryDisplay" : document.getElementById('primary-display'),
+    "secondaryDisplay" : document.getElementById('secondary-display')
 });
 
-document.getElementById('btn-addition').addEventListener('click', function() {
-    add();
-});
-
-// Add listener for user keydown arrow keys
-document.addEventListener('keydown', (event)  => {
-    console.log(event.keyCode);
-
-    switch(event.keyCode) {
-        // 48 - 57
-        // Numbers 0-9
-        case 48:
-            input(0);
-            break;
-        case 49:
-            input(1);
-            break;
-        case 50:
-            input(2);
-            break;
-        case 51:
-            input(3);
-            break;
-        case 52:
-            input(4);
-            break;
-        case 53:
-            input(5);
-            break;
-        case 54:
-            input(6);
-            break;
-        case 55:
-            input(7);
-            break;
-        case 56:
-            input(8);
-            break;
-        case 57:
-            input(9);
-            break;
-        case 187:
-            add();
-            break;
-        case 191:
-            divide();
-            break;
-    }
-});
-
-function input(value) {
-    document.getElementById('number').innerText = (document.getElementById('number').innerText === '0')
-        ? value.toString()
-        : document.getElementById('number').innerText.toString() + value.toString();
-
-    document.getElementById('sub-number').innerText = (document.getElementById('sub-number').innerText === '0')
-        ? value.toString()
-        : document.getElementById('sub-number').innerText.toString() + value.toString();
-}
-
-function add() {
-    document.getElementById('number').innerText = '0';
-    document.getElementById('sub-number').innerText = document.getElementById('sub-number').innerText + ' + ';
-}
-
-function subtract() {
-    document.getElementById('number').innerText = '0';
-    document.getElementById('sub-number').innerText = document.getElementById('sub-number').innerText + ' - ';
-}
-
-function multiply() {
-    document.getElementById('number').innerText = '0';
-    document.getElementById('sub-number').innerText = document.getElementById('sub-number').innerText + ' * ';
-}
-
-function divide() {
-    document.getElementById('number').innerText = '0';
-    document.getElementById('sub-number').innerText = document.getElementById('sub-number').innerText + ' / ';
-}
