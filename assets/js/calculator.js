7ffac1e632b792fedea607228ed47a374917628c
Richard Taylor Dawson <richard.taylor.dawson@gmail.com>
2018-11-25 17:15:46 -0700

    Monospace font added, comment blocks, more functionality, etc

--- assets/js/calculator.js
+++ assets/js/calculator.js
@@ -1,8 +1,15 @@
 // TODO Use Module Export for JS Class
-// TODO Add comment blocks
+/**
+ * @author Richard Dawson
+ * @classdesc involves all logic, event listeners, etc of a working calculator application
+ */
 class Calculator {
-    constructor(elements) {
-        // Number buttons zero indexed 0-9
+    /**
+     * Create a Calculator.
+     * @param {object} elements - All the DOM objects of the calculator.
+     * @param {object} options - All the possible options & settings for the calculator.
+     */
+    constructor(elements, options) {
         this.numberButtons = elements.numberButtons;
 
         this.decimalButton = elements.decimalButton;
@@ -19,13 +26,17 @@ class Calculator {
         this.primaryDisplay = elements.primaryDisplay;
         this.secondaryDisplay = elements.secondaryDisplay;
 
+        this.maxNumberLength = options.maxNumberLength;
+
         this.initializeClickEvents();
 
         this.initializeKeyboardEvents();
     }
 
+    /**
+     * Adds click event handlers to all DOM objects of the calculator.
+     */
     initializeClickEvents() {
-        // Numbers 0 - 9
         for(let i = 0; i < this.numberButtons.length; i++) {
             this.numberButtons[i].addEventListener('click', (event) => {
                 this.input(this.getInputValue(event.target));
@@ -61,8 +72,10 @@ class Calculator {
         });
     }
 
+    /**
+     * Adds a keydown handler to the document that listens for certain keystrokes used for calculations
+     */
     initializeKeyboardEvents() {
-        // TODO Implement Delete Keyboard Button
         document.addEventListener('keydown', (event)  => {
             console.log(event.keyCode);
 
@@ -70,34 +83,34 @@ class Calculator {
                 // 48 - 57
                 // Numbers 0 - 9
                 case 48:
-                    this.input(0);
+                    this.input('0');
                     break;
                 case 49:
-                    this.input(1);
+                    this.input('1');
                     break;
                 case 50:
-                    this.input(2);
+                    this.input('2');
                     break;
                 case 51:
-                    this.input(3);
+                    this.input('3');
                     break;
                 case 52:
-                    this.input(4);
+                    this.input('4');
                     break;
                 case 53:
-                    this.input(5);
+                    this.input('5');
                     break;
                 case 54:
-                    this.input(6);
+                    this.input('6');
                     break;
                 case 55:
-                    this.input(7);
+                    this.input('7');
                     break;
                 case 56:
-                    this.input(8);
+                    this.input('8');
                     break;
                 case 57:
-                    this.input(9);
+                    this.input('9');
                     break;
                 // . or decimal key
                 case 190:
@@ -127,84 +140,146 @@ class Calculator {
         });
     }
 
-    getInputValue(element) {
-        return this.numberButtons.indexOf(element);
+    /**
+     * Enables or disables all number buttons and decimal button
+     * @param {bool} enable - Decides whether or not to enable or disable the buttons.
+     */
+    toggleNumberButtons(enable) {
+        for(let i = 0; i < this.numberButtons.length; i++) {
+            this.numberButtons[i].disabled = enable;
+        }
+
+        this.decimalButton.disabled = enable;
     }
 
+    /**
+     * Searches and finds the input value of a number button
+     * @param {element} element - DOM element that was clicked or keystroked.
+     * @return Returns the index of the matched element
+     */
+    getInputValue(element) {
+        return this.numberButtons.indexOf(element).toString();
+    }
+
+    /**
+     * Adds the input value to the primary and secondary display of the calculator
+     * @param {string} value - Either a number value between 0-9 or a decimal value
+     */
     input(value) {
-        const decimalRegex = new RegExp(/\./, 'g');
-        const operatorRegex = new RegExp(/[\/\+\-\*]/, 'g');
+        if(this.primaryDisplay.innerText.toString().length !== this.maxNumberLength) {
+            const decimalRegex = new RegExp(/\./, 'g');
+            const operatorRegex = new RegExp(/[\/\+\-\*]/, 'g');
 
-        // Check to make sure a decimal hasn't already been added since numbers can only have a single decimal point
-        if((value === '.' && !decimalRegex.test(this.primaryDisplay.innerText)) || value !== '.') {
-            let secondaryDisplayValue = (operatorRegex.test(this.secondaryDisplay.innerText) && this.secondaryDisplay.innerText.split(' ').length !== 3)
-                ? this.secondaryDisplay.innerText.toString() + ' ' + value.toString()
-                : this.secondaryDisplay.innerText.toString() + value.toString();
+            // Check to make sure a decimal hasn't already been added since numbers can only have a single decimal point
+            if((value === '.' && !decimalRegex.test(this.primaryDisplay.innerText)) || value !== '.') {
+                let secondaryDisplayValue = (operatorRegex.test(this.secondaryDisplay.innerText) && this.secondaryDisplay.innerText.split(' ').length !== 3)
+                    ? `${this.secondaryDisplay.innerText.toString()} ${value.toString()}`
+                    : `${this.secondaryDisplay.innerText.toString()}${value.toString()}`;
 
-            this.primaryDisplay.innerText = (this.primaryDisplay.innerText !== '0' && value !== '.') || (value === '.')
-                ? this.primaryDisplay.innerText.toString() + value.toString()
-                : value.toString();
+                this.primaryDisplay.innerText = (this.primaryDisplay.innerText !== '0' && value !== '.') || (value === '.')
+                    ? `${this.primaryDisplay.innerText.toString()}${value.toString()}`
+                    : value.toString();
 
-            //TODO Need to figure out the secondary display better
-            this.secondaryDisplay.innerText = (this.secondaryDisplay.innerText !== '0' && value !== '.') || (value === '.')
-                ? secondaryDisplayValue
-                : value.toString();
+                //TODO Need to figure out the secondary display better
+                this.secondaryDisplay.innerText = (this.secondaryDisplay.innerText !== '0' && value !== '.') || (value === '.')
+                    ? secondaryDisplayValue
+                    : value.toString();
+            }
+
+            this.toggleNumberButtons(this.primaryDisplay.innerText.toString().length === this.maxNumberLength)
         }
     }
 
+    /**
+     * Clears the calculator displays and resets everything back to its original state
+     */
     clear() {
         this.primaryDisplay.innerText = '0';
         this.secondaryDisplay.innerText = '0';
+        this.toggleNumberButtons(false);
     }
 
+    /**
+     * Lets the class know that addition would like to be performed by either calling computer or adding
+     * to the secondary display of the calculator.
+     */
     add() {
-        const additionRegex = new RegExp(/\+/, 'g');
-
-        if(!additionRegex.test(this.secondaryDisplay.innerText)) {
-            this.primaryDisplay.innerText = '0';
-            this.secondaryDisplay.innerText = this.secondaryDisplay.innerText + ' + ';
-        } else {
-            //Do computation and call add again
-        }
+        // const operatorRegex = new RegExp(/[\/\+\-\*]/, 'g');
+        //
+        // if(!operatorRegex.test(this.secondaryDisplay.innerText)) {
+        //     this.primaryDisplay.innerText = '0';
+        //     this.secondaryDisplay.innerText = this.secondaryDisplay.innerText + ' + ';
+        // } else {
+        //     //Do computation and call add again
+        //     this.compute(true);
+        // }
+        this.primaryDisplay.innerText = '0';
+        this.secondaryDisplay.innerText = this.secondaryDisplay.innerText + ' + ';
     }
 
+    /**
+     * Lets the class know that subtraction would like to be performed by either calling computer or adding
+     * to the secondary display of the calculator.
+     */
     subtract() {
         this.primaryDisplay.innerText = '0';
         this.secondaryDisplay.innerText = this.secondaryDisplay.innerText + ' - ';
     }
 
+    /**
+     * Lets the class know that multiplication would like to be performed by either calling computer or adding
+     * to the secondary display of the calculator.
+     */
     multiply() {
         this.primaryDisplay.innerText = '0';
         this.secondaryDisplay.innerText = this.secondaryDisplay.innerText + ' * ';
     }
 
+    /**
+     * Lets the class know that division would like to be performed by either calling computer or adding
+     * to the secondary display of the calculator.
+     */
     divide() {
         this.primaryDisplay.innerText = '0';
         this.secondaryDisplay.innerText = this.secondaryDisplay.innerText + ' / ';
     }
 
-    compute() {
-        const parts = this.secondaryDisplay.innerText.split(' ');
+    /**
+     * Computes the mathematical expression and displays it on the calculator
+     * @param {bool} early - If a computation button is clicked before equals, then computation occurs "early"
+     */
+    compute(early = false) {
+        const equalsRegex = new RegExp(/=/, 'g');
 
-        let value;
+        if(!equalsRegex.test(this.secondaryDisplay.innerText)) {
+            const parts = this.secondaryDisplay.innerText.split(' ');
 
-        switch(parts[1]) {
-            case "+":
-                value = parseInt(parts[0]) + parseInt(parts[2]);
-                break;
-            case "-":
-                value = parseInt(parts[0]) - parseInt(parts[2]);
-                break;
-            case "*":
-                value = parseInt(parts[0]) * parseInt(parts[2]);
-                break;
-            case "/":
-                value = parseInt(parts[0]) / parseInt(parts[2]);
-                break;
+            let value;
+
+            switch(parts[1]) {
+                case "+":
+                    value = parseInt(parts[0]) + parseInt(parts[2]);
+                    break;
+                case "-":
+                    value = parseInt(parts[0]) - parseInt(parts[2]);
+                    break;
+                case "*":
+                    value = parseInt(parts[0]) * parseInt(parts[2]);
+                    break;
+                case "/":
+                    value = parseInt(parts[0]) / parseInt(parts[2]);
+                    break;
+            }
+
+            this.primaryDisplay.innerText = (early)
+                ? '0'
+                : value;
+
+            this.secondaryDisplay.innerText = (early)
+                ? `${value} ${parts[1]}`
+                : `${this.secondaryDisplay.innerText.toString()} = `;
+        } else {
+
         }
-
-        this.primaryDisplay.innerText = value;
-        this.secondaryDisplay.innerText = this.secondaryDisplay.innerText.toString() + ' = ' + value.toString();
     }
-
 }
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,24 +1,34 @@
-const calculator = new Calculator({
-    "numberButtons": [
-        document.getElementById('zeroButton'),
-        document.getElementById('oneButton'),
-        document.getElementById('twoButton'),
-        document.getElementById('threeButton'),
-        document.getElementById('fourButton'),
-        document.getElementById('fiveButton'),
-        document.getElementById('sixButton'),
-        document.getElementById('sevenButton'),
-        document.getElementById('eightButton'),
-        document.getElementById('nineButton')
-    ],
-    "decimalButton" : document.getElementById('btn-decimal'),
-    "clearButton" : document.getElementById('btn-clear'),
-    "additionButton" : document.getElementById('btn-addition'),
-    "subtractionButton" : document.getElementById('btn-subtraction'),
-    "multiplicationButton" : document.getElementById('btn-multiplication'),
-    "divisionButton" : document.getElementById('btn-division'),
-    "equalsButton" : document.getElementById('btn-equals'),
-    "primaryDisplay" : document.getElementById('primary-display'),
-    "secondaryDisplay" : document.getElementById('secondary-display')
-});
+/**
+ * Initializes the calculator object
+ * Object takes care of all the logic, event listeners, etc of the calculator
+ * @constant calculator
+ */
+const calculator = new Calculator(
+    {
+        "numberButtons": [
+            document.getElementById('zeroButton'),
+            document.getElementById('oneButton'),
+            document.getElementById('twoButton'),
+            document.getElementById('threeButton'),
+            document.getElementById('fourButton'),
+            document.getElementById('fiveButton'),
+            document.getElementById('sixButton'),
+            document.getElementById('sevenButton'),
+            document.getElementById('eightButton'),
+            document.getElementById('nineButton')
+        ],
+        "decimalButton" : document.getElementById('btn-decimal'),
+        "clearButton" : document.getElementById('btn-clear'),
+        "additionButton" : document.getElementById('btn-addition'),
+        "subtractionButton" : document.getElementById('btn-subtraction'),
+        "multiplicationButton" : document.getElementById('btn-multiplication'),
+        "divisionButton" : document.getElementById('btn-division'),
+        "equalsButton" : document.getElementById('btn-equals'),
+        "primaryDisplay" : document.getElementById('primary-display'),
+        "secondaryDisplay" : document.getElementById('secondary-display')
+    },
+    {
+        "maxNumberLength" : 12
+    }
+);
 
