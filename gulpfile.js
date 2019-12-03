348204013152bfce3a22edef8bedbcbbd3edc2fd
Richard Taylor Dawson <richard.taylor.dawson@gmail.com>
2019-12-02 20:41:34 -0700

    updating build process

--- gulpfile.js
+++ gulpfile.js
@@ -1,87 +1,75 @@
-const gulp = require('gulp');
-const browserSync = require('browser-sync').create();
-const imagemin = require('gulp-imagemin');
-const cache = require('gulp-cache');
-const del = require('del');
-const runSequence = require('run-sequence');
-const cleanCSS = require('gulp-clean-css');
-const uglify = require('gulp-uglify');
-const htmlmin = require('gulp-htmlmin');
-const rollup = require('gulp-better-rollup');
-const babel = require('rollup-plugin-babel');
+const gulp = require('gulp')
+const browserSync = require('browser-sync').create()
+const imagemin = require('gulp-imagemin')
+const cache = require('gulp-cache')
+const del = require('del')
+const runSequence = require('run-sequence')
+const cleanCSS = require('gulp-clean-css')
+const uglify = require('gulp-uglify')
+const htmlmin = require('gulp-htmlmin')
+const rollup = require('gulp-better-rollup')
+const babel = require('rollup-plugin-babel')
 
-gulp.task('css', function() {
-  return gulp.src('_src/css/**/*.css')
+gulp.task('css', () =>
+  gulp.src('_src/css/index.css')
     .pipe(cleanCSS())
     .pipe(gulp.dest('dist/css'))
-    .pipe(browserSync.reload({
-      stream: true
-    }))
-});
+    .pipe(browserSync.reload({ stream: true }))
+)
 
-gulp.task('images', function(){
-  return gulp.src('_src/images/**/*.+(png|jpg|jpeg|gif|svg)')
+gulp.task('images', () => {
+  gulp.src('_src/images/**/*.+(png|jpg|jpeg|gif|svg)')
     .pipe(cache(imagemin()))
     .pipe(gulp.dest('dist/images'))
-});
 
-gulp.task('js', function() {
-  return gulp.src('_src/js/index.js')
-      .pipe(rollup({
-        plugins: [babel()]
-      }, {
-        format: 'cjs',
-      }))
+  return del.sync('dist/images')
+})
+
+gulp.task('js', () =>
+  gulp.src('_src/js/index.js')
+    .pipe(rollup({ plugins: [babel()]}, { format: 'cjs' }))
     .pipe(uglify())
     .pipe(gulp.dest('dist/js'))
-    .pipe(browserSync.reload({
-      stream: true
-    }))
-});
+    .pipe(browserSync.reload({ stream: true }))
+)
 
-gulp.task('html', function() {
-  return gulp.src('_src/**/*.html')
+gulp.task('html', () => {
+  gulp.src('_src/**/*.html')
     .pipe(htmlmin({ collapseWhitespace: true }))
     .pipe(gulp.dest('dist/'))
-    .pipe(browserSync.reload({
-      stream: true
-    }))
-});
+    .pipe(browserSync.reload({ stream: true }))
 
-gulp.task('files', function() {
-  return gulp.src('_src/**/*.+(json|txt|xml)')
+  return del.sync('dist/**/*.html')
+})
+
+gulp.task('files', () => {
+  gulp.src('_src/**/*.+(json|txt|xml)')
     .pipe(gulp.dest('dist/'))
-});
 
-gulp.task('clean:dist', function() {
-  return del.sync('dist');
-});
+  return del.sync('dist/**/*.+(json|txt|xml)')
+})
 
-gulp.task('browserSync', function() {
-  browserSync.init({
-    server: {
-      baseDir: './dist'
-    },
-  })
-});
+gulp.task('syncDist', () => del.sync('dist'))
 
-gulp.task('watch', function() {
-  gulp.watch('_src/css/**/*.css', ['css', 'clean:dist']);
-  gulp.watch('_src/images/**/*.+(png|jpg|jpeg|gif|svg)', ['images', 'clean:dist']);
-  gulp.watch('_src/js/**/*.js', ['js', 'clean:dist']);
-  gulp.watch('_src/**/*.html', ['html', 'clean:dist']);
-  gulp.watch('_src/**/*.+(json|txt|xml)', ['files', 'clean:dist']);
-});
+gulp.task('browserSync', () => browserSync.init({ server: { baseDir: './dist' } }))
 
-gulp.task('build', function (callback) {
-  runSequence('clean:dist',
-    ['css', 'images', 'js', 'html', 'files'],
+gulp.task('watch', () => {
+  gulp.watch('_src/css/**/*.css', ['css'])
+  gulp.watch('_src/images/**/*.+(png|jpg|jpeg|gif|svg)', ['images'])
+  gulp.watch('_src/js/**/*.js', ['js'])
+  gulp.watch('_src/**/*.html', ['html'])
+  gulp.watch('_src/**/*.+(json|txt|xml)', ['files'])
+})
+
+gulp.task('build', (callback) => {
+  runSequence(['css', 'images', 'js', 'html', 'files'],
     callback
   )
-});
+})
 
-gulp.task('default', function (callback) {
-  runSequence(['css', 'images', 'js', 'html', 'files', 'clean:dist', 'browserSync', 'watch'],
+gulp.task('default', (callback) => {
+  runSequence('syncDist',
+    ['css', 'images', 'js', 'html', 'files', 'browserSync', 'watch'],
     callback
   )
-});
+})
