18a7b3fac926caefa8e6036c5abc4c81d32232e9
Richard Taylor Dawson <richard.taylor.dawson@gmail.com>
2021-01-03 13:17:38 -0700

    adding mobile styles and removing console logs

--- _src/js/index.js
+++ _src/js/index.js
@@ -101,54 +101,50 @@ if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("/sw.js")
 }
 
-document.getElementById("install-button").addEventListener("click", () => {
-  document.querySelector("pwa-install").openPrompt()
-})
+const installBtn = document.getElementById("install-button")
 
-const setShowInstallBtn = (showBtn) => {
-  if (showBtn) {
-    document.getElementById("install-button").style.display = "block"
-  } else {
-    document.getElementById("install-button").style.display = "none"
+if (installBtn !== null) {
+  installBtn.addEventListener("click", () => {
+    document.querySelector("pwa-install").openPrompt()
+  })
+
+  const setShowInstallBtn = (showBtn) => {
+    if (showBtn) {
+      installBtn.style.display = "block"
+    } else {
+      installBtn.style.display = "none"
+    }
   }
-}
 
-const isIOS =
-  navigator.userAgent.includes("iPhone") ||
-  navigator.userAgent.includes("iPad") ||
-  (navigator.userAgent.includes("Macintosh") &&
-    typeof navigator.maxTouchPoints === "number" &&
-    navigator.maxTouchPoints > 2)
+  const isIOS =
+    navigator.userAgent.includes("iPhone") ||
+    navigator.userAgent.includes("iPad") ||
+    (navigator.userAgent.includes("Macintosh") &&
+      typeof navigator.maxTouchPoints === "number" &&
+      navigator.maxTouchPoints > 2)
 
-const isSupportingBrowser = window.hasOwnProperty("BeforeInstallPromptEvent")
+  const isSupportingBrowser = window.hasOwnProperty("BeforeInstallPromptEvent")
 
-const isStandalone = window.matchMedia("(display-mode: standalone)").matches
+  const isStandalone = window.matchMedia("(display-mode: standalone)").matches
 
-if (isStandalone) {
-  setShowInstallBtn(false)
-} else {
-  console.log(
-    (isIOS && isSupportingBrowser) ||
-      (isSupportingBrowser &&
-        (localStorage.getItem("calculatorInstalled") === "" ||
-          localStorage.getItem("calculatorInstalled") === "false"))
-  )
+  if (isStandalone) {
+    setShowInstallBtn(false)
+  } else {
+    setShowInstallBtn(
+      (isIOS && isSupportingBrowser) ||
+        (isSupportingBrowser &&
+          (localStorage.getItem("calculatorInstalled") === "" ||
+            localStorage.getItem("calculatorInstalled") === "false"))
+    )
 
-  setShowInstallBtn(
-    (isIOS && isSupportingBrowser) ||
-      (isSupportingBrowser &&
-        (localStorage.getItem("calculatorInstalled") === "" ||
-          localStorage.getItem("calculatorInstalled") === "false"))
-  )
+    // This will only be called if the browser is eligible and PWA has NOT been installed yet
+    window.addEventListener("beforeinstallprompt", () => {
+      localStorage.setItem("calculatorInstalled", "false")
+      setShowInstallBtn(true)
+    })
 
-  // This will only be called if the browser is eligible and PWA has NOT been installed yet
-  window.addEventListener("beforeinstallprompt", () => {
-    localStorage.setItem("calculatorInstalled", "false")
-    setShowInstallBtn(true)
-    console.log("beforeinstallprompt")
-  })
-
-  window.addEventListener("appinstalled", () => {
-    localStorage.setItem("calculatorInstalled", "true")
-  })
+    window.addEventListener("appinstalled", () => {
+      localStorage.setItem("calculatorInstalled", "true")
+    })
+  }
 }
