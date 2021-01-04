680d0c30fee516d0344d1ad5d226d6422f2cdf67
Richard Taylor Dawson <richard.taylor.dawson@gmail.com>
2021-01-03 21:21:16 -0700

    switching out sandstone theme

--- _src/js/index.js
+++ _src/js/index.js
@@ -73,8 +73,8 @@ const theme = new Theme({
       default: false,
     },
     {
-      element: document.getElementById("sandstone-theme"),
-      identifier: "sandstone-theme",
+      element: document.getElementById("pulse-theme"),
+      identifier: "pulse-theme",
       default: false,
     },
     {
